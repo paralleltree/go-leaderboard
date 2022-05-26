5990f31dfcef89295ac09920419b1ef61cb518dd
Paralleltree <paralleltree@outlook.com>
2022-05-26 22:13:54 +0900

    Add converting camel case keys interceptor for axios

--- web/src/api_client.ts
+++ web/src/api_client.ts
@@ -1,8 +1,20 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
+import camelCaseKeys from "camelcase-keys";
+
 import { EventModel } from './models/event';
 
 export class ApiClient {
+  private axiosInstance;
+
   constructor(private endpoint: string) {
+    this.axiosInstance = axios.create({
+      headers: { "Content-Type": "application/json" },
+      responseType: "json",
+    });
+    this.axiosInstance.interceptors.response.use((res: AxiosResponse): AxiosResponse => {
+      res.data = camelCaseKeys(res.data, { deep: true });
+      return res;
+    });
   }
 
   private buildUrl(path: string): string {
@@ -11,7 +23,7 @@ export class ApiClient {
 
   async getEvents(page: number): Promise<EventModel[]> {
     const url = this.buildUrl(`/events?page=${page}&count=20`);
-    const res = await axios.get<EventModel[]>(url);
+    const res = await this.axiosInstance.get<EventModel[]>(url);
     return res.data;
   }
 }
